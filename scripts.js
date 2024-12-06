9d8c102dce0ef014d1865aa48c8f3d22eb741f7f
NECK <wahtsitoyou@gmail.com>
2024-12-06 15:49:20 -0700

    More Movies

--- scripts.js
+++ scripts.js
@@ -9,35 +9,46 @@ const apiKey = 'f6f18c676cae5a2f43248f81247dde67';
 
 // EXAMPLE of what API key looks like below: 
 // 'c3cf1225c677e4a8152e8957bc30d8edd';
+/* const apiUrl = `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`; */
+/* const moviesContainer = document.getElementById("movies"); */
 
-const apiUrl = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}`;
-const moviesContainer = document.getElementById("movies");
 
-async function fetchMovies() {
+export async function fetchMovies(input,containerId) {
+    console.log(input, containerId)
+    const apiUrl = `https://api.themoviedb.org/3/trending/all/${input}?api_key=${apiKey}`;
+    
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
 
-        data.results.forEach(media => {
+        // dynamic set up
+        const targetContainer = document.getElementById(containerId);
+        
+        if(!targetContainer){
+            console.error(`Container with id "${containerId}" not found.`);
+        }
+
+        //clear existing container contents
+        targetContainer.innerHTML = '';
+
+        data.results.slice(0,4).forEach(media => {
             const movieCard = createMovieCard(media);
-            moviesContainer.appendChild(movieCard);
+            targetContainer.appendChild(movieCard);
         });
-    } catch (error) {
-        console.error("Error fetching data:", error);
+    }catch (error) {
+        console.error("Error fetching dat:", error);
     }
 }
 
 function createMovieCard(media) {
-    const { title, name, backdrop_path } = media;
+    const { title, name, poster_path } = media;
 
     const movieCard = document.createElement("div");
     movieCard.classList.add("movie_item")
 
     movieCard.innerHTML = `
-        <img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" class="movie_img_rounded">
+        <img src="https://image.tmdb.org/t/p/w500/${poster_path}" class="movie_img">
         <div class = "title">${title || name}</div>
     `;
     return movieCard;
 }
-
-fetchMovies();
