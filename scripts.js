cee43494f7c7e16df23f8b76a2e8c5c1cc0ac7cf
NECK <wahtsitoyou@gmail.com>
2024-12-09 16:50:59 -0700

    Blur

--- scripts.js
+++ scripts.js
@@ -1,17 +1,4 @@
-// Watch my Movie Database API Project Tutorial video here: https://youtu.be/hOeR3LB9NJY - The Movie Database API Project Tutorial using HTML, CSS, and JavaScript
-
-
-// Use your own MovieDB API Key below
-// Watch my Movie Database API Project Tutorial video here: https://youtu.be/hOeR3LB9NJY to get your own API key for free!
-
 const apiKey = 'f6f18c676cae5a2f43248f81247dde67';
-// uncomment above line once you put your own api key above
-
-// EXAMPLE of what API key looks like below: 
-// 'c3cf1225c677e4a8152e8957bc30d8edd';
-/* const apiUrl = `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`; */
-/* const moviesContainer = document.getElementById("movies"); */
-
 
 export async function fetchMovies(input,containerId) {
     console.log(input, containerId)
